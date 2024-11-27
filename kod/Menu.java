bb42ff88fd8e2f0bc75ddd6daf3a7ab0a3ff83f9
pavloslobodian <slobodian@stud.uniza.sk>
2024-11-27 12:41:03 +0100

    Pridanie tried Tryska a Menu

---
+++ kod/Menu.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class Menu here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Menu {
+    /**
+     * Constructor for objects of class Menu
+     */
+    public Menu() {
+        // initialise instance variables
+    }
+}
