ea3bb615aeb044b64cb1eec1b25816fa116d9351
Benjamín Collu <benjamincollu@gmail.com>
2024-11-27 13:05:44 +0100

    vytvorenie triedy ManazerGravitacie

---
+++ kod/ManazerGravitacie.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class ManazerGravitacie here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class ManazerGravitacie {
+    /**
+     * Constructor for objects of class ManazerGravitacie
+     */
+    // private Mapa mapa;
+    // private Kvapalina kvapalina
+    public ManazerGravitacie() {
+        // initialise instance variables
+    }
+}
