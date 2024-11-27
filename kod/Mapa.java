19877b97b70c68c3e9fa1f594966f79f12ee3cf5
Martin Kortiš <martinkortis29@gmail.com>
2024-11-27 13:09:13 +0100

    Mapa V1

---
+++ kod/Mapa.java
@@ -0,0 +1,28 @@
+import fri.shapesge.Obdlznik;
+import java.util.ArrayList;
+/**
+ * Write a description of class Mapa here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Mapa {
+    /**
+     * Constructor for objects of class Mapa
+     */
+    private Obdlznik podlaha;
+    /*
+    private Tryska tryska1;
+    private Tryska tryska2;
+    private Tryska tryska3;
+    */
+            
+    public Mapa() {
+        // initialise instance variables
+    }
+    /*
+    public ArrayList<Tryska> getTrysky() {
+        return null;
+    }
+    */
+}
