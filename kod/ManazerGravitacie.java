c3bb8f928f9174b324c3b3ebaeead33a023e05f4
Benjamín Collu <benjamincollu@gmail.com>
2024-11-29 23:18:38 +0100

    Implementacia triedy ManazerGravitacie podla UML

--- kod/ManazerGravitacie.java
+++ kod/ManazerGravitacie.java
@@ -9,9 +9,13 @@ public class ManazerGravitacie {
     /**
      * Constructor for objects of class ManazerGravitacie
      */
-    // private Mapa mapa;
-    // private Kvapalina kvapalina
-    public ManazerGravitacie() {
+    private Mapa mapa;
+    private Kvapalina kvapalina;
+    public ManazerGravitacie(Mapa mapa, Kvapalina kvapalina) {
         // initialise instance variables
+        this.mapa = mapa;
+        this.kvapalina = kvapalina;
     }
+    
+    public void tik() {}
 }
