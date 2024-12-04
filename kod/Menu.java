c3cbc49632babce607500a2bf4567deb303bd4ef
pavloslobodian <slobodian@stud.uniza.sk>
2024-12-04 18:07:20 +0100

    Vôbec som neprišiel na to, ako implementovať
    tlačidlá, ale možno budem musieť pridať metódu
    do triedy sand, ktorej bude metóda menu niečo predávať,
    na základe čoho sa nakonfiguruje materiál alebo niečo iné.

--- kod/Menu.java
+++ kod/Menu.java
@@ -11,11 +11,39 @@ public class Menu {
     private Obdlznik voda;
     private Obrazok pozadie;
     
-    public Menu() {
+    public Menu(String pathToImage) {
+        int pressedButton= 0;
+        this.pozadie = new Obrazok(pathToImage);
+        this.pozadie.zmenPolohu(0, 0);
         
+        
+        this.voda = new Obdlznik();
+        this.voda.zmenPolohu(100, 100);
+        this.voda.zmenStrany(600, 200);
+        this.voda.zmenFarbu("blue");
+        
+        
+        this.piesok = new Obdlznik();
+        this.piesok.zmenPolohu(100, 500);
+        this.voda.zmenStrany(600, 200);
+        this.piesok.zmenFarbu("yellow");
+        
+        
+        this.pozadie.zobraz();
+        this.voda.zobraz();
+        this.piesok.zobraz();
+        this.vyberKvapalinu(pressedButton);
     }
     
-    public void vyberKvaoalinu(String kvapalina){
-    
+    public void vyberKvapalinu(int pressedButton){
+        switch(pressedButton){
+            case 1:
+            //tu musi byt metoda triedy sand ktora ukaze co dalej robit
+                return;
+            case 2:
+            //tu musi byt metoda triedy sand ktora ukaze co dalej robit
+                return;
+        }
+        return;
     }
 }
