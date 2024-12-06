737541cc7083276f637d4eb61f48ef328d061a0a
Benjamín Collu <benjamincollu@gmail.com>
2024-12-06 15:09:26 +0100

    Pridanie funkcionality stlacenia tlacidiel, docasneho menu obrazka, pridanie funkcionality mysi do sbge.ini

--- kod/Menu.java
+++ kod/Menu.java
@@ -1,5 +1,6 @@
 import fri.shapesge.Obdlznik;
 import fri.shapesge.Obrazok;
+import javafx.geometry.Rectangle2D;
 /**
  * Write a description of class Menu here.
  * 
@@ -10,40 +11,41 @@ public class Menu {
     private Obdlznik piesok;
     private Obdlznik voda;
     private Obrazok pozadie;
+    private Rectangle2D hitboxPiesok;
+    private Rectangle2D hitboxVoda;
+    private int vybranaKvapalina;
     
-    public Menu(String pathToImage) {
-        int pressedButton= 0;
-        this.pozadie = new Obrazok(pathToImage);
+    public Menu() {
+        this.vybranaKvapalina = 0;
+        this.pozadie = new Obrazok("img/pozadie.png");
         this.pozadie.zmenPolohu(0, 0);
-        
-        
+
         this.voda = new Obdlznik();
         this.voda.zmenPolohu(100, 100);
-        this.voda.zmenStrany(600, 200);
+        this.voda.zmenStrany(50, 20);
         this.voda.zmenFarbu("blue");
-        
+        this.hitboxVoda = new Rectangle2D(100, 10, 50, 20);
         
         this.piesok = new Obdlznik();
-        this.piesok.zmenPolohu(100, 500);
-        this.voda.zmenStrany(600, 200);
+        this.piesok.zmenPolohu(300, 100);
+        this.piesok.zmenStrany(50, 20);
         this.piesok.zmenFarbu("yellow");
-        
+        this.hitboxVoda = new Rectangle2D(300, 100, 50, 20);
         
         this.pozadie.zobraz();
-        this.voda.zobraz();
-        this.piesok.zobraz();
-        this.vyberKvapalinu(pressedButton);
     }
     
-    public void vyberKvapalinu(int pressedButton){
-        switch(pressedButton){
-            case 1:
-            //tu musi byt metoda triedy sand ktora ukaze co dalej robit
-                return;
-            case 2:
-            //tu musi byt metoda triedy sand ktora ukaze co dalej robit
-                return;
+    public int getVybranaKvapalina() {
+        return this.vybranaKvapalina;
+    }
+    
+    public void stlacenieTlacidla(int x, int y) {
+        Rectangle2D mys = new Rectangle2D(x, y, 1, 1);
+        if(mys.intersects(this.hitboxPiesok)) {
+            this.vybranaKvapalina = 1;
         }
-        return;
+        if(mys.intersects(this.hitboxVoda)) {
+            this.vybranaKvapalina = 2;
+        }
     }
 }
