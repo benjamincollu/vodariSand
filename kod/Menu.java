8a35ed55ba3d6a22a727dbb64335f505fc6710b6
pavloslobodian <slobodian@stud.uniza.sk>
2024-12-11 12:51:25 +0100

    Doplnenie Menu

--- kod/Menu.java
+++ kod/Menu.java
@@ -1,4 +1,5 @@
 import fri.shapesge.Obdlznik;
+import fri.shapesge.Obrazok;
 import fri.shapesge.BlokTextu;
 import javafx.geometry.Rectangle2D;
 import fri.shapesge.StylFontu;
@@ -9,13 +10,15 @@ import fri.shapesge.StylFontu;
  * @version (a version number or a date)
  */
 public class Menu {
-    
+    private Obrazok pozadie;
     private Obdlznik piesok;
     private Obdlznik voda;
     private Rectangle2D hitboxPiesok;
     private Rectangle2D hitboxVoda;
     private int vybranaKvapalina;
     private BlokTextu nazovHry;
+    private BlokTextu vyberteText;
+    private BlokTextu creditsText;
     /**
      * Constructor for objects of class Menu
      */
@@ -23,34 +26,51 @@ public class Menu {
         // initialise instance variables
         this.vybranaKvapalina = 0;
         
-        this.nazovHry = new BlokTextu("SAND", 125, 100);
-        this.nazovHry.zmenFont("Arial", StylFontu.BOLD, 50);
+        this.pozadie = new Obrazok("img/pozadie.png", 0, 0);
+        this.pozadie.zobraz();
+        
+        this.nazovHry = new BlokTextu("SAND", 400 - 140, 125);
+        this.nazovHry.zmenFont("Arial", StylFontu.BOLD, 100);
         this.nazovHry.zobraz();
         
+        this.vyberteText = new BlokTextu("Vyberte materiál", 400 - 110, 225);
+        this.vyberteText.zmenFont("Arial", StylFontu.BOLD, 25);
+        this.vyberteText.zobraz();
+        
+        this.creditsText = new BlokTextu("Vodári - Benjamín Collu, Marek Kuzma, Martin Kortiš, Tomáš Kučera, Pavlo Slobodian", 5, 590);
+        this.creditsText.zmenFont("Arial", StylFontu.BOLD, 15);
+        this.creditsText.zobraz();
+        
         this.piesok = new Obdlznik();
-        this.piesok.zmenPolohu(100, 200);
-        this.piesok.zmenStrany(50, 20);
+        this.piesok.zmenPolohu(175, 275);
+        this.piesok.zmenStrany(150, 50);
         this.piesok.zmenFarbu("yellow");
-        this.hitboxPiesok = new Rectangle2D(100, 200, 50, 20);
+        this.hitboxPiesok = new Rectangle2D(175, 275, 150, 50);
         this.piesok.zobraz();
         
         this.voda = new Obdlznik();
-        this.voda.zmenPolohu(250, 200);
-        this.voda.zmenStrany(50, 20);
+        this.voda.zmenPolohu(475, 275);
+        this.voda.zmenStrany(150, 50);
         this.voda.zmenFarbu("blue");
-        this.hitboxVoda = new Rectangle2D(250, 200, 50, 20);
+        this.hitboxVoda = new Rectangle2D(475, 275, 150, 50);
         this.voda.zobraz();
     }
     
     public void skry() {
+        this.pozadie.skry();
+        this.pozadie = null;
         this.nazovHry.skry();
         this.nazovHry = null;
+        this.vyberteText.skry();
+        this.vyberteText = null;
         this.voda.skry();
         this.hitboxVoda = null;
         this.voda = null;
         this.piesok.skry();
         this.hitboxPiesok = null;
         this.piesok = null;
+        this.creditsText.skry();
+        this.creditsText = null;
     }
     
     public int getVybranaKvapalina() {
@@ -58,11 +78,13 @@ public class Menu {
     }
     
     public void stlacenieTlacidla(int x, int y) {
-        if(this.hitboxPiesok.contains(x,y)) {
-            this.vybranaKvapalina = 1;
-        }
-        if(this.hitboxVoda.contains(x,y)) {
-            this.vybranaKvapalina = 2;
+        if(this.vybranaKvapalina == 0) {
+            if (this.hitboxPiesok.contains(x,y)) {
+                this.vybranaKvapalina = 1;
+            }
+            if (this.hitboxVoda.contains(x,y)) {
+                this.vybranaKvapalina = 2;
+            }
         }
     }
 }
