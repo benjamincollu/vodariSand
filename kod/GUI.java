903bd30ca0bcbe2d18efe26a8e7f6a7ff477987f
Martin Kortiš <martinkortis29@gmail.com>
2024-12-13 22:14:26 +0100

    Pridanie dokumentacnych komentarov a umoznenie zmeny rychlosti a sance

--- kod/GUI.java
+++ kod/GUI.java
@@ -4,52 +4,133 @@ import fri.shapesge.StylFontu;
 import fri.shapesge.Obdlznik;
 import javafx.geometry.Rectangle2D;
 /**
- * Write a description of class GUI here.
+ * Vykresľuje interaktívne grafické rozhranie
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Martin Kortiš
+ * @version 13.12.2024
  */
 public class GUI {
-    private Obrazok meter;
+    private Obdlznik meter;
     private BlokTextu sekundy;
     private BlokTextu materialNazov;
     private BlokTextu materialHustota;
     private Obdlznik vysypTlacidlo;
+    private Obdlznik zmenRychlostTlacidlo;
+    private Obdlznik zmenSancuTlacidlo;
     private BlokTextu vysypText;
+    private BlokTextu zmenRychlostText;
+    private BlokTextu rychlostText;
+    private BlokTextu zmenSancuText;
+    private BlokTextu sancaText;
     private float cas;
+    private int rychlost;
+    private int sanca;
     /**
-     * Constructor for objects of class GUI
+     * Inicializuje všetky grafické prvky
+     * @param Material material vstupný druh materiálu, aký vypísať
      */
     public GUI(Material material) {
         // initialise instance variables
-        this.meter = new Obrazok("img/meter.png", 0, 0);
+        rychlost = 1;
+        sanca = 50;
+        
+        this.meter = new Obdlznik(500, 0);
+        this.meter.zmenFarbu("black");
+        this.meter.zmenStrany(5, 600);
         this.meter.zobraz();
         this.sekundy = new BlokTextu("0", 2, 20);
         this.sekundy.zmenFont("Arial", StylFontu.BOLD, 20);
         this.sekundy.zobraz();
         
-        this.materialNazov = new BlokTextu(String.format("Materiál: %s", material.toString()), 505, 25);
+        this.materialNazov = new BlokTextu(String.format("Materiál: %s", material.toString()), 515, 25);
         this.materialNazov.zmenFont("Arial", StylFontu.BOLD, 20);
         this.materialNazov.zobraz();
         
-        this.materialHustota = new BlokTextu(String.format("hustota: %#.2f", material.getHustota()), 505, 50);
+        this.materialHustota = new BlokTextu(String.format("hustota: %#.2f", material.getHustota()), 515, 50);
         this.materialHustota.zmenFont("Arial", StylFontu.BOLD, 20);
         this.materialHustota.zobraz();
         
-        this.vysypTlacidlo = new Obdlznik(550, 125);
+        this.rychlostText = new BlokTextu(String.format("rychlosť: %d", rychlost), 515, 75);
+        this.rychlostText.zmenFont("Arial", StylFontu.BOLD, 20);
+        this.rychlostText.zobraz();
+        
+        this.sancaText = new BlokTextu(String.format("šanca: %d%%", 50), 515, 100);
+        this.sancaText.zmenFont("Arial", StylFontu.BOLD, 20);
+        this.sancaText.zobraz();
+        
+        // tlacidla
+        this.vysypTlacidlo = new Obdlznik(550, 250);
         this.vysypTlacidlo.zmenStrany(200, 50);
         this.vysypTlacidlo.zobraz();
         
-        this.vysypText = new BlokTextu("Vysyp", 595, 160);
-        this.vysypText.zmenFont("Arial", StylFontu.BOLD, 35);
+        this.vysypText = new BlokTextu("Vysyp", 605, 285);
+        this.vysypText.zmenFont("Arial", StylFontu.BOLD, 30);
         this.vysypText.zmenFarbu("#FFFFFFF");
         this.vysypText.zobraz();
+        
+        this.zmenRychlostTlacidlo = new Obdlznik(550, 350);
+        this.zmenRychlostTlacidlo.zmenStrany(200, 50);
+        this.zmenRychlostTlacidlo.zobraz();
+        
+        this.zmenRychlostText = new BlokTextu("Zmeniť rýchlosť", 575, 380);
+        this.zmenRychlostText.zmenFont("Arial", StylFontu.BOLD, 20);
+        this.zmenRychlostText.zmenFarbu("#FFFFFFF");
+        this.zmenRychlostText.zobraz();
+        
+        this.zmenSancuTlacidlo = new Obdlznik(550, 450);
+        this.zmenSancuTlacidlo.zmenStrany(200, 50);
+        this.zmenSancuTlacidlo.zobraz();
+        
+        this.zmenSancuText = new BlokTextu("Zmeniť šancu", 585, 480);
+        this.zmenSancuText.zmenFont("Arial", StylFontu.BOLD, 20);
+        this.zmenSancuText.zmenFarbu("#FFFFFFF");
+        this.zmenSancuText.zobraz();
+    }
+
+    
+    /**
+     * Aktualizuje rýchlosť na základe parametra
+     * @param int novaRychlost celočíselná nová rýchlosť
+     */
+    public void setRychlost(int novaRychlost) {
+        this.rychlost = novaRychlost;
     }
     
+    /**
+     * Aktualizuje šancu na základe parametra
+     * @param int novaSanca celočíselná nová šanca
+     */
+    public void setSanca(int novaSanca) {
+        this.sanca = novaSanca;
+    }
+    
+    /**
+     * Vráti Rectangle2D s hranicou tlačidla Vysyp
+     * @return Rectangle2D hranica
+     */
     public Rectangle2D getVysypHitbox() {
-        return new Rectangle2D(550, 125, 200, 50);
+        return new Rectangle2D(550, 250, 200, 50);
     }
     
+    /**
+     * Vráti Rectangle2D s hranicou tlačidla Zmeniť rýchlosť
+     * @return Rectangle2D hranica
+     */
+    public Rectangle2D getZmenRychlostHitbox() {
+        return new Rectangle2D(550, 350, 200, 50);
+    }
+    
+    /**
+     * Vráti Rectangle2D s hranicou tlačidla Zmeniť šancu
+     * @return Rectangle2D hranica
+     */
+    public Rectangle2D getSancaHitbox() {
+        return new Rectangle2D(550, 450, 200, 50);
+    }
+    
+    /**
+     * Skryje a vynuluje všetky prvky pre šetrenie pamäte
+     */
     public void skry() {
         this.meter.skry();
         this.meter = null;
@@ -61,10 +142,27 @@ public class GUI {
         this.materialHustota = null;
         this.vysypTlacidlo.skry();
         this.vysypTlacidlo = null;
+        this.rychlostText.skry();
+        this.rychlostText = null;
+        this.zmenRychlostText.skry();
+        this.zmenRychlostText = null;
+        this.zmenRychlostTlacidlo.skry();
+        this.zmenRychlostTlacidlo = null;
+        this.zmenSancuText.skry();
+        this.zmenSancuText = null;
+        this.zmenSancuTlacidlo.skry();
+        this.zmenSancuTlacidlo = null;
+        this.sancaText.skry();
+        this.sancaText = null;
     }
     
+    /**
+     * Aktualizuje grafické prvky
+     */
     public void tik() {
         this.cas += 0.05;
         this.sekundy.zmenText(String.format("%d", (int)this.cas));
+        this.rychlostText.zmenText(String.format("rychlosť: %d", rychlost));
+        this.sancaText.zmenText(String.format("šanca: %d%%", this.sanca));
     }
 }
