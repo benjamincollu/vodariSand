b0e33e23118fa64413bca43d2f77bef32c2b3465
Benjamín Collu <benjamincollu@gmail.com>
2024-12-15 11:31:02 +0100

    Uprava UML v blueJ, uprava Trysky, pridanie dokumentačných komentárov

--- kod/Tryska.java
+++ kod/Tryska.java
@@ -1,19 +1,20 @@
 import fri.shapesge.Stvorec;
 /**
- * Write a description of class Tryska here.
+ * Vytvára grafickú reprezentáciu trysky
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Pavlo Slobodian
+ * @version 13.12.2024
  */
 public class Tryska {
-    
     private Stvorec stvorec;
     private int polohaX;
     private int polohaY;
     private static final String FARBA = "#8a5d04";
     private static final int VELKOST_TRYSKY = 25;
     /**
-     * Constructor for objects of class Tryska
+     * Inicializuje štvorec na zadanej pozícii podľa parametrov
+     * @param int polohaX pozícia na osi X
+     * @param int polohaY pozícia na osi Y
      */
     public Tryska(int polohaX, int polohaY) {
         this.polohaX = polohaX;
@@ -24,14 +25,33 @@ public class Tryska {
         this.stvorec.zobraz();
     }
     
+    /**
+     * Skryje štvorec
+     */
+    public void skry() {
+        this.stvorec.skry();
+    }
+    
+    /**
+     * Vráti pozíciu na osi X
+     * @return celé číslo
+     */
     public int getPolohaX() {
         return this.polohaX;
     }
     
+    /**
+     * Vráti pozíciu na osi Y
+     * @return celé číslo
+     */
     public int getPolohaY() {
         return this.polohaY + VELKOST_TRYSKY;
     }
     
+    /**
+     * Vráti konštantu veľkosť trysky
+     * @return celé číslo
+     */
     public int getVelkostTrysky() {
         return VELKOST_TRYSKY;
     }
