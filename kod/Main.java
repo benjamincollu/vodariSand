6273b012e50f946908db15904e6f47f0e40102b7
Benjamín Collu <benjamincollu@gmail.com>
2024-12-15 11:35:25 +0100

    Vytvorenie triedy main

---
+++ kod/Main.java
@@ -0,0 +1,16 @@
+import java.awt.geom.Rectangle2D;
+/**
+ * Hlavná spúšťacia trieda Main
+ * 
+ * @author Benjamín Collu
+ * @version 13.12.2024
+ */
+public class Main {
+    /**
+     * Funkcia spúšťaná .jar súborom
+     */
+    public static void main(String[] args) {
+        // vytvorí novú inštanciu hry
+        Sand hra = new Sand();
+    }
+}
